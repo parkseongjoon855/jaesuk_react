498e35cc5378646f79659d83dba3e88d6e8da29e
parkseongjoon855 <taxy9399@gmail.com>
2025-07-07 12:00:51 +0900

    유재석소개페이지

---
+++ src/components/Intro.jsx
@@ -0,0 +1,13 @@
+import suk3 from '../assets/images/jaeseok3.png'
+function Intro(){
+    return(<>
+        <h1>소개</h1>
+        <p>1972년 8월 14일</p>
+        <p>서울특별시 성북구 수유동 출생</p>
+        <p>국적 : 대한민국 / 종교 : 불교</p>
+        <p>178cm, 65kg, B형, 270mm, ISFP</p>
+        <p>서울예술전문대학 방송연예과 중퇴</p>
+        <img src={suk3} alt=""></img>
+    </>)
+}
+export default Intro;
